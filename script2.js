d9d7337fb022db9e3d032eb64e2c5537e08c0b8b
patricio11-loki <61088358+patricio11-loki@users.noreply.github.com>
2024-11-13 21:22:42 -0500

    .

--- script2.js
+++ script2.js
@@ -2,5 +2,5 @@ let numero=parseInt(prompt("Ingrese un número"));
 if(numero % 2==0){
     document.write("numero es par")
 }else{
-    document.write("nuemero es impar");
+    document.write("numero es impar");
 }
